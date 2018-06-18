10a6b8163f8e1dbff4d61813e18dcc213477bb93
jorgiom4 <gorkalqy@hotmail.com>
2018-06-18 21:53:29 +0200

    Añadimos ejercicios con cadenas

--- src/com/instancia2/Main.java
+++ src/com/instancia2/Main.java
@@ -2,10 +2,81 @@ package com.instancia2;
 
 /**
  * Tema 3 Java SDK API
+ *
  */
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        String s1 = "Ejemplo de cadena";
+        String s2 = "Ejemplo de cadena mas larga";
+        String s3 = "Ejemplo de cadena mas larga todavia mas";
+        String s4 = "Ejemplo de cadena mas larga todavia mas y mas";
+        */
+
+        String cadenas[] =  new String[4];
+
+        cadenas[0] = "Ejemplo de cadena";
+        cadenas[1] = "Ejemplo de cadena mas largaererererer er er re er ere r er";
+        cadenas[2] = "Ejemplo de cadena mas larga todavia mas";
+        cadenas[3] = "Ejemplo de cadena mas larga todavia mas y mas";
+
+        int mayor=0;
+        int leng=0;
+
+        String cadenaMayor = "";
+
+        for(int i=0;i<3;i++){
+            leng = cadenas[i].length();
+            if(leng > mayor){
+                mayor = leng;
+                cadenaMayor = "cadena[" + (i) + "]";
+            }
+
+        }
+
+        System.out.println("mayor: " + mayor + " " + cadenaMayor);
+
     }
+
+    /**
+     * Obtenemos el numero de vocales en una cadena
+     */
+    public static void getVocales(){
+
+        String vocales[] = {"a", "e", "i", "o", "u"};
+
+        String cadena = "Esto es una cadena con muchas vocales";
+
+        int numeroValores = 0;
+
+        for(int i=0;i<cadena.length();i++){
+            switch (cadena.toLowerCase().charAt(i)){
+                case 'a':
+
+                case 'e':
+
+                case'i':
+
+                case 'o':
+
+                case 'u':
+                    //Si paso por aqui es vocal
+                    numeroValores ++;
+                default:
+                    continue;
+
+            }
+        }
+
+        System.out.println("El numero de vocales es:" + numeroValores);
+
+
+
+
+
+    }
+
+
 }
